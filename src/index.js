26fc4c51d338a7b8e039cfb767a3fea566c9bb3a
RCCarr <95515731+RCCarr@users.noreply.github.com>
2023-05-16 22:19:09 +0100

    added live time data in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
+setInterval(function () {
+  //dublin
 
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+setInterval(function () {
+  //paris
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
+
+setInterval(function () {
+  //fiji
+
+  let fijiElement = document.querySelector("#fiji");
+  let fijiDateElement = fijiElement.querySelector(".date");
+  let fijiTimeElement = fijiElement.querySelector(".time");
+  let fijiTime = moment().tz("Pacific/Fiji");
+
+  fijiDateElement.innerHTML = fijiTime.format("MMMM Do YYYY");
+  fijiTimeElement.innerHTML = fijiTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
